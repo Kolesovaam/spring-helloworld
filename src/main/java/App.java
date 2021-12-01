d802c852199b08446bfd8522af17c5499a3a8490
Мария Колесова <mari-kolesova@inbox.ru>
2021-12-01 21:49:24 +0300

    push

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 public class App {
     public static void main(String[] args) {
@@ -7,6 +8,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+        System.out.println(bean == bean2);
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean3.getName());
+        System.out.println(bean4.getName());
+        System.out.println(bean3 == bean4);
     }
+
 }
